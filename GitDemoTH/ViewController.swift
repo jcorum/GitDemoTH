67f03cb903a0f5819fc446ec910d2acc15959458
Jim Corum <jcorum@james-corum.local>
2016-03-03 17:07:44 -0800

    Initial Commit

---
+++ GitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoTH
+//
+//  Created by Jim Corum on 3/3/16.
+//  Copyright © 2016 Jim Corum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
