623ebb453336d113f3d3c3d2cbd5aa396b39eab3
jcorum <jcorum@users.noreply.github.com>
2016-03-03 17:43:10 -0800

    Update ViewController.swift

--- GitDemoTH/ViewController.swift
+++ GitDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "Hello Dummy, Dummy"
+    let userName = "Hello Dummy, Dummy, Dummy"
 
     override func viewDidLoad() {
         super.viewDidLoad()
